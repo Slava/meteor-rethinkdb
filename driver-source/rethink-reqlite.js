bbc087295b87519d8134ae4e9cc387f437e6ade0
Slava Kim <slava@meteor.com>
2015-04-27 02:11:19 -0700

    A query wrapper to always run queries on the test database

--- driver-source/rethink-reqlite.js
+++ driver-source/rethink-reqlite.js
@@ -1,4 +1,4 @@
-module.exports = {
+var e = {
   Table: require('reqlite/lib/table.js'),
   Database: require('reqlite/lib/database.js'),
   'Document': require('reqlite/lib/document.js'),
@@ -8,8 +8,19 @@ module.exports = {
     return {
       databases: {
         'test': (new this.Database('test'))
+      },
+      version: e.protoDef.VersionDummy.Version.V0_4,
+      protocol: e.protoDef.VersionDummy.Protocol.JSON,
+      runQuery: function (q) {
+        var self = this;
+        var Tt = e.protoDef.Term.TermType;
+        var Qt = e.protoDef.Query.QueryType;
+        var extendedQuery = [Qt.START, q, {db:[Tt.DB, ["test"]]}];
+        return new e.Query(self, extendedQuery).run();
       }
     };
   }
 };
+
+module.exports = e;
 
